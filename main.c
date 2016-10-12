cc8fd202d547a72d78ccd6921b261404949eb4f4
Niklas Königsson <1982konig@gmail.com>
2016-10-12 17:26:00 +0200

    cxgxf

--- main.c
+++ main.c
@@ -133,6 +133,7 @@ int main(void){
     int pid;
 
     do{
+        fflush(stderr);
         fprintf(stderr, "mish%% ");
         fflush(stderr);
         getline(&inputLine, &line_size, stdin);
@@ -177,8 +178,6 @@ int main(void){
             pipe(fd);
             //the first one will take input from stdin and the rest from the
             //read-end of the pipe
-            printf("%d,%d ", fd[0], fd[1]);
-            fflush(stdout);
             pid = forkProcess(fd, in, fd[1], &comLine[index]);
             if(close(fd[1]) < 0){
                 perror("close error:");
@@ -202,6 +201,7 @@ int main(void){
             }
             execvp(comLine[index].argv[0], comLine[index].argv);
             fprintf(stderr,"no such command\n");
+            fflush(stderr);
             exit(EXIT_FAILURE);
         }
 
@@ -229,6 +229,7 @@ int forkProcess (int fd[2], int in, int out, command *cmd) {
         //execute the command
         execvp(cmd->argv[0], cmd->argv);
         fprintf(stderr,"no such command");
+        fflush(stderr);
         exit(EXIT_FAILURE);
     }
 
